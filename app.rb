09e1d89075e7c0d32107aedb529246ecd2a6725d
JayIssuree <jay_issuree@hotmail.co.uk>
2021-02-15 20:39:51 +0000

    extracted hmtl to views folder

--- app.rb
+++ app.rb
@@ -1,15 +1,13 @@
 require 'sinatra'
 
 get '/' do
-    "Hello world"
+    erb(:index)
 end
 
 get '/secret' do
-    "This is a secret message"
+    erb(:secret)
 end
 
 get '/cat' do
-    "<html>
-        <img src='https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697' width=850 height=600 style='border: 3px dashed red'>
-    <html>"
+    erb(:cat)
 end
