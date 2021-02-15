6965388bda6ba5a138a8ee1b77440b9fd50fd9f8
JayIssuree <jay_issuree@hotmail.co.uk>
2021-02-15 20:34:26 +0000

    added cat image with minor css styling

--- app.rb
+++ app.rb
@@ -7,3 +7,9 @@ end
 get '/secret' do
     "This is a secret message"
 end
+
+get '/cat' do
+    "<html>
+        <img src='https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697' width=850 height=600 style='border: 3px dashed red'>
+    <html>"
+end
