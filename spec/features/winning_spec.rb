9b16981bce7691adbc9edd64e09d5879c98f8c82
JayIssuree <jay_issuree@hotmail.co.uk>
2021-03-08 16:00:12 +0000

    added a play again button once the game has finished

--- spec/features/winning_spec.rb
+++ spec/features/winning_spec.rb
@@ -9,4 +9,16 @@ describe "wining", type: :feature do
         expect(page).to have_content("Player 2 loses!")
     end
 
+    it "has a replay button once the game has ended" do
+        sign_in_and_play
+        click_button "HeavyAttack"
+        allow(Game.current_game).to receive(:finished?).and_return(:true)
+        allow(Game.current_game).to receive(:loser).and_return(Game.current_game.player2)
+        click_button "Return"
+        expect(page).to have_button("Play again")
+        click_button("Play again")
+        expect(page).to have_field("player_1")
+        expect(page).to have_field("player_2")
+    end
+
 end
